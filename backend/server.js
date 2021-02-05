940e91cca5dce958f11dad8b14f31b6ceef4c09f
Steven T <45646017+storres86@users.noreply.github.com>
2021-02-04 21:51:07 -0800

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        // origin: "http://localhost:3000",
+        origin: "https://storres86.github.io/",
         credentials: true,
     })
 );
